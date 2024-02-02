769381a15b338d10a20e9097ee51b08d6620a5df
MaximeNlr <maxime.neller@gmail.com>
2024-02-02 17:00:34 +0100

    creation tableau + js

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-let nom = document.querySelector(".nom");
-let prixVente = document.querySelector(".prixVente");
-let prixAchat = document.querySelector(".prixAchat");
-let type = document.querySelector(".type");
-let recherche = document.querySelector(".recherche");
+let searchBarre = document.querySelector(".searchBarre");
+let creationProduit = document.querySelector(".creationProduit");
+let typeProduit = document.querySelector(".typeProduit");
 let ajouter = document.querySelector(".ajouter");
-let gestionArticle = document.querySelector(".gestionArticle");
+let listeArticle = document.querySelector(".listeArticle");
+let bodyTable = document.querySelector(".bodyTable");
 
-let produits = [];
 
 
-function fiche(ficheArticle) {
-    let quantite = document.createElement("input");
-    ficheArticle.appendChild(quantite);   
-}
 
 ajouter.addEventListener("click", function (event) {
     event.preventDefault();
-     
-    const ficheArticle = document.createElement("li");
-    gestionArticle.appendChild(ficheArticle);
-    fiche(ficheArticle);
+    const creaProduitValue = creationProduit.value;
+    const ficheArticleContainer = document.createElement("tr");
+    const ficheArticle = document.createElement("td");
+    const prixAchat = document.createElement("td");
+    const prixVente = document.createElement("td");
+    const marge = document.createElement("td");
+    const prixVenteTTC = document.createElement("td");
+    const typeProduit = document.createElement("td");
 
+    ficheArticle.innerHTML = (creaProduitValue);
+    prixAchat.innerHTML =("<input type =number>");
+    prixVente.innerHTML =("<input type =number>");
+    marge.innerHTML =("<input type =number>");
+    prixVenteTTC.innerHTML =("<input type =number>");
+    typeProduit.innerHTML =("<select><option value =>Choisir type</option><option value =ba>Boisson alcoolisée</option><option value =bna>Boisson non alcoolisée</option></select>")
+    
+    bodyTable.appendChild(ficheArticleContainer);
+    ficheArticleContainer.appendChild(ficheArticle);
+    ficheArticleContainer.appendChild(prixAchat);
+    ficheArticleContainer.appendChild(prixVente);
+    ficheArticleContainer.appendChild(marge);
+    ficheArticleContainer.appendChild(prixVenteTTC);
+    ficheArticleContainer.appendChild(typeProduit);
+
+
+
+
+
+    
 })
-
-
 
 
