29de4303da5714a87718ba8492906f0ebaf89470
MaximeNlr <maxime.neller@gmail.com>
2024-02-02 13:36:25 +0100

    second branch

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 let nom = document.querySelector(".nom");
-let quantite = document.querySelector(".quantite");
 let prixVente = document.querySelector(".prixVente");
 let prixAchat = document.querySelector(".prixAchat");
 let type = document.querySelector(".type");
 let recherche = document.querySelector(".recherche");
 let ajouter = document.querySelector(".ajouter");
-let stockContainer = document.querySelector(".stockContainer");
+let gestionArticle = document.querySelector(".gestionArticle");
 
 let produits = [];
 
 
 function fiche(ficheArticle) {
-    ficheArticle.style.height = ("100px")
-    ficheArticle.style.background = ("black");
-    
+    let quantite = document.createElement("input");
+    ficheArticle.appendChild(quantite);   
 }
 
 ajouter.addEventListener("click", function (event) {
     event.preventDefault();
-    const nomValue = nom.Value;
-    const prixVenteValue = prixVente.value;
-    const prixAchatValue = prixAchat.value;
-    const typeValue = type.value;
-    const stockContainerValue = stockContainer.value; 
-    const ficheArticle = document.createElement("section");
-    stockContainer.appendChild(ficheArticle);
+     
+    const ficheArticle = document.createElement("li");
+    gestionArticle.appendChild(ficheArticle);
+    fiche(ficheArticle);
 
-   
-    
 })
 
 
